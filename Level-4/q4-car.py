497afdece9bb5735df5cf42c50bc6bcc188ec11e
Pradeep Kumar Gupta ( Alven Alex ) <MRYASHFTP@GMAIL.COM>
2026-04-16 20:42:59 +0530

    Update q4-car.py

--- Level-4/q4-car.py
+++ Level-4/q4-car.py
@@ -1,18 +1,20 @@
-class Student :
-    def __init__(self,name,model,price):
-        self.name=name
-        self.model=model
-        self.price=price
+class Car:
+    def __init__(self, name, model, price):
+        self.name = name
+        self.model = model
+        self.price = price
+
     def show(self):
         print("Car Name:", self.name)
         print("Car Model:", self.model)
-        print("Priece:", self.price)
+        print("Price:", self.price)
+
     def discount(self):
-        amount=int(self.price)*0.1
+        amount = self.price * 0.1
         self.price -= amount
-        print("Discount ",amount)
-v=Student("Lamborghini ","Revuelto",807000)
+        print("Discount:", amount)
+
+v = Car("Lamborghini", "Revuelto", 807000)
 v.show()
 v.discount()
 v.show()
-
