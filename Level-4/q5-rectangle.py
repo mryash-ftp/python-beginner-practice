6b4ce21676744fd107549c93492c7d6eabb3f91e
Pradeep Kumar Gupta ( Alven Alex ) <MRYASHFTP@GMAIL.COM>
2026-04-16 20:43:41 +0530

    Update q5-rectangle.py

--- Level-4/q5-rectangle.py
+++ Level-4/q5-rectangle.py
@@ -1,1 +1,14 @@
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
 
+    def area(self):
+        print("Area:", self.length * self.breadth)
+
+    def perimeter(self):
+        print("Perimeter:", 2 * (self.length + self.breadth))
+
+v = Rectangle(20, 50)
+v.area()
+v.perimeter()
