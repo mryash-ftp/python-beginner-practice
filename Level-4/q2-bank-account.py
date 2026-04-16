3846ca49e3bfb882644fbb557bbb13588be04632
Pradeep Kumar Gupta ( Alven Alex ) <MRYASHFTP@GMAIL.COM>
2026-04-16 20:45:54 +0530

    Update q2-bank-account.py

--- Level-4/q2-bank-account.py
+++ Level-4/q2-bank-account.py
@@ -9,27 +9,23 @@ class BankAccount:
         print("Account No:", self.account_no)
         print("Account Balance:", self.balance)
 
-    def aeps(self):
-        choice = int(input("[1]: Withdrawal\n[2]: Deposit\nEnter your choice: "))
+    def menu(self):
+        choice = int(input("[1] Withdrawal\n[2] Deposit\nEnter your choice: "))
         if choice == 1:
             amount = int(input("Enter Withdrawal Amt: "))
             if amount <= self.balance:
                 self.balance -= amount
-                print("-" * 40)
                 print("Withdrawal Successful")
-                print("-" * 40)
             else:
                 print("Insufficient balance")
         elif choice == 2:
             amount = int(input("Enter Deposit Amt: "))
             self.balance += amount
-            print("-" * 40)
             print("Deposit Successful")
-            print("-" * 40)
         else:
             print("Invalid choice")
 
 v1 = BankAccount("State Bank Of India", "59841865xxx99", 10000)
 v1.show()
-v1.aeps()
+v1.menu()
 v1.show()
