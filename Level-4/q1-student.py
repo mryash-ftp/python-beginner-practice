799274284332a6d99fc37f3fabc27acd6300a6a1
Pradeep Kumar Gupta ( Alven Alex ) <MRYASHFTP@GMAIL.COM>
2026-04-16 20:46:05 +0530

    Update q1-student.py

--- Level-4/q1-student.py
+++ Level-4/q1-student.py
@@ -1,17 +1,20 @@
-class Student :
-    def __init__(self,name,roll_no,marks):
-        self.name=name
-        self.roll_no=roll_no
-        self.marks=marks
+class Student:
+    def __init__(self, name, roll_no, marks):
+        self.name = name
+        self.roll_no = roll_no
+        self.marks = marks
+
     def show(self):
-        print("Name",self.name)
-        print("Roll No:",self.roll_no)
-        print("Marks:",self.marks)
+        print("Name:", self.name)
+        print("Roll No:", self.roll_no)
+        print("Marks:", self.marks)
+
     def result(self):
-        if self.marks>=33:
-            print("Result:","Pass")
+        if self.marks >= 33:
+            print("Result: Pass")
         else:
-            print("Result:","Fail")
-v=Student("Alven","AB8972SX",75)
+            print("Result: Fail")
+
+v = Student("Alven", "AB8972SX", 75)
 v.show()
 v.result()
