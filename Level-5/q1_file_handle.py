d4103b3e2adf879ba6d963b82966ad74be6df26f
Pradeep Kumar Gupta ( Alven Alex ) <MRYASHFTP@GMAIL.COM>
2026-04-18 07:39:13 +0530

    Create q1_file_handle.py

---
+++ Level-5/q1_file_handle.py
@@ -0,0 +1,6 @@
+with open("data.txt","w") as f:
+    f.write(f"python most easy language!\n")
+    f.write(f"python what is your secret!\n")
+    f.write(f"Someone Hate me !\n")
+    f.write(f"Someone one love me!\n")
+print("Data Saved")
