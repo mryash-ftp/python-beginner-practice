58caa9c9f699c646b03d600b1ab6e79348f1ca66
Pradeep Kumar Gupta ( Alven Alex ) <MRYASHFTP@GMAIL.COM>
2026-04-22 13:08:39 +0530

    Add files via upload

---
+++ Practice_Day/Day_1/10- Revise.py
@@ -0,0 +1,2 @@
+# Created By Alven Alex
+# Date: 2026-04-22 08:24
---
+++ Practice_Day/Day_1/9- Revise.py
@@ -0,0 +1,10 @@
+n=int(input("Enter a number: "))
+if n<2:
+    print("Not a prime number")
+else:
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            print("Not a prime number")
+            break;
+    else:
+        print("Prime number")
