431a7168525f044a3458d719947a4074f90d8f5a
Pradeep Kumar Gupta ( Alven Alex ) <MRYASHFTP@GMAIL.COM>
2026-04-25 21:25:51 +0530

    Update sprint_strings.py

--- break_limit/Mega-Sprint-50/sprint_strings.py
+++ break_limit/Mega-Sprint-50/sprint_strings.py
@@ -1,1 +1,17 @@
+def analysis(user):
+    count=0
+    for i in user.lower():
+        if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+            count+=1
+    print(f"Number of vowels in the sentence: {count}")
+    uppcount=0
+    lowcount=0
+    for i in user:
+        if i.isupper():
+            uppcount+=1
+        elif i.islower():
+            lowcount+=1
+
+    print(f"Number of uppercase letters: {uppcount}")
+    print(f"Number of lowercase letters: {lowcount}")
 
