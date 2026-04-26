fbdee37129692035a777aa4de746190a24606949
Pradeep Kumar Gupta ( Alven Alex ) <MRYASHFTP@GMAIL.COM>
2026-04-26 11:56:28 +0530

    Create sprint_lvl4_.py

---
+++ break_limit/Mega-Sprint-50/sprint_lvl4_.py
@@ -0,0 +1,21 @@
+import datetime as dt
+
+def utility():
+    a=dt.datetime.now()
+    print(a.strftime("Date: %d-%m-%y | Time : %I:%H:%S"))
+    if a.year%4==0 or a.year%100==0 and a%400==0:
+        print("Leap Year")
+    else:
+        print("Not Leap Year")
+def converter(c) :
+    f=c*(9/5+32)
+    print("Fahrenheit",f)
+def Asci(n):
+    if n.isalpha():
+        print("ASCII Value Of",n,":",ord(n))
+    else:
+        print("Enter only letters")
+user=input("Enter a number: ")  
+utility()
+converter(10)
+Asci(user)
